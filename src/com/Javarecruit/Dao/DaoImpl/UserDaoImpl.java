bd8afb3e469924fd5acd29e618e94212502d959f
admin <1735381788@qq.com>
2022-07-12 17:11:09 +0800

    新增语句

--- src/com/Javarecruit/Dao/DaoImpl/UserDaoImpl.java
+++ src/com/Javarecruit/Dao/DaoImpl/UserDaoImpl.java
@@ -1,11 +1,21 @@
 package com.Javarecruit.Dao.DaoImpl;
 
+import com.Javarecruit.Dao.BaseDao;
 import com.Javarecruit.Dao.UserDao;
+import com.Javarecruit.pojo.User;
 
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends BaseDao implements UserDao {
     @Override
     public int revise() {
         String sql = "update user set upwd = ?,phone = ?,marke";
         return 0;
+    }
+
+    @Override
+    public int addByUser(User user) {
+        String sql = "insert into user values(uname,upwd,phone,mark,email,nid,study,job,sex,address,personal)";
+        Object[] objects={user.getUname(),user.getUpwd(),user.getPhone(),user.getMark(),user.getEmail(),user.getNid(),user.getStudy(),user.getJob(),user.getSex(),user.getAddress(),user.getPersonal()};
+        int insert = exceuteUpdate(sql, objects);
+        return insert;
     }
 }
--- src/com/Javarecruit/Dao/UserDao.java
+++ src/com/Javarecruit/Dao/UserDao.java
@@ -1,9 +1,18 @@
 package com.Javarecruit.Dao;
 
+import com.Javarecruit.pojo.User;
+
 public interface UserDao {
     /**
      * 修改
      * @return 返回确认数值
      */
     int revise();
+
+    /**
+     * 新增
+     * @param user 用户
+     * @return 返回受影响行数
+     */
+    int addByUser(User user);
 }
