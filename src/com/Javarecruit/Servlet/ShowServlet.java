6cb61738aa754a9a6163dcad0fee192f67c67e06
zhanghaofei1 <746921214@qq.com>
2022-07-12 20:06:07 +0800

    展示

---
+++ src/com/Javarecruit/Servlet/ShowServlet.java
@@ -0,0 +1,34 @@
+package com.Javarecruit.Servlet;
+
+import com.Javarecruit.Service.ServiceImpl.ShowServiceImpl;
+import com.Javarecruit.pojo.Show;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "ShowServlet",urlPatterns = {"/Test"})
+public class ShowServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        PrintWriter out=response.getWriter();
+        String name=request.getParameter("title");
+        ShowServiceImpl show=new ShowServiceImpl();
+        List<Show> ssh=show.showAll();
+        request.setAttribute("show",ssh);
+        System.out.println("asd");
+        request.getRequestDispatcher("showMore.jsp").forward(request,response);
+        out.flush();
+        out.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
