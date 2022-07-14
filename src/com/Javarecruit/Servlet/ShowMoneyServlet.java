a82d216e24c13f08daae3ff0becc65d40145cc6d
Exception <452693066@qq.com>
2022-07-14 11:09:32 +0800

    IMPL

--- src/com/Javarecruit/Servlet/ShowMoneyServlet.java
+++ src/com/Javarecruit/Servlet/ShowMoneyServlet.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @WebServlet(name = "ShowMoneyServlet",urlPatterns = {"/ShowMoneyServlet"})
 public class ShowMoneyServlet extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
@@ -31,7 +32,7 @@ public class ShowMoneyServlet extends HttpServlet {
         out.flush();
         out.close();
     }
-
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request,response);
     }
