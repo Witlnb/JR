6dcf2250754efd2bc37a1903e3d040c2618ca938
lx20001125 <3542603699@qq.com>
2022-07-14 20:15:43 +0800

    发布招聘信息1.1

--- src/com/Javarecruit/Servlet/ShowAddServlet.java
+++ src/com/Javarecruit/Servlet/ShowAddServlet.java
@@ -16,6 +16,7 @@ import java.io.PrintWriter;
 public class ShowAddServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         PrintWriter out=response.getWriter();
         String title = request.getParameter("title");
