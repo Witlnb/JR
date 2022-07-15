cfb80d27c434cc0e0e2cb1ca88721a67801a3803
zhanghaofei1 <746921214@qq.com>
2022-07-15 15:04:55 +0800

    展示

--- web/JS/Hall.js
+++ web/JS/Hall.js
@@ -13,9 +13,8 @@ $(document).on('click', '.iop', function () {
                 "data":"iop="+$(this).text(),
                 "dataType":"text",
                 "success" :function (){
-                    // window.location="Hall.jsp";
-                    $("#ShowAll").load(location.href+"#ShowAll");
-                 },
+                    window.location="Hall.jsp";
+                    },
     })
 })
 function search(value) {
@@ -30,6 +29,41 @@ function search(value) {
 $(document).click(function (e) {
     if ('iput' != e.target.className) {
         $('.op-list').addClass('hidden');
+        var a = $('hidden').val();
+    }
+});
+//标签的展示
+function showTT() {
+    $('.showTitle').toggleClass('hidden');
+    $('.st').show();
+}
+$(document).on('click', '.st', function () {
+    $('.showTitle').addClass('hidden');
+    var text = $(this).text();
+    $('.title').val(text);
+    $.ajax({
+        "url":"TitleTest",
+        "type":"get",
+        "async":false,
+        "data":"st="+$(this).text(),
+        "dataType":"text",
+        "success" :function (){
+            window.location="Hall.jsp";
+},
+    })
+})
+function test(value) {
+    $('.st').show();
+    $('.st').each(function () {
+        var text = $(this).text();
+        if (text.indexOf(value) == -1) {
+            $(this).hide();
+        }
+    });
+}
+$(document).click(function (e) {
+    if ('title' != e.target.className) {
+        $('.showTitle').addClass('hidden');
         var a = $('hidden').val();
     }
 });
