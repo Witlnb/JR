a87fafcc0f14d38a4bb70cfdf8bd969b39fc0437
lx20001125 <3542603699@qq.com>
2022-07-17 14:36:39 +0800

    Merge branch 'master' of D:\JavaProject with conflicts.

---
+++ src/com/Javarecruit/Dao/DaoImpl/CommentDaoImpl.java
@@ -0,0 +1,12 @@
+package com.Javarecruit.Dao.DaoImpl;
+
+import com.Javarecruit.Dao.BaseDao;
+import com.Javarecruit.Dao.CommentDao;
+
+public class CommentDaoImpl extends BaseDao implements  CommentDao {
+    @Override
+    public int addcomment(CommentDao c) {
+
+        return 0;
+    }
+}
